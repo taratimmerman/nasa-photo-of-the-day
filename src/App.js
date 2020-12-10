68f2071c12ffb8d325c7b55df4ecc1aa405b5cfa
taratimmerman <timmermantara@gmail.com>
2020-12-09 17:43:04 -0700

    Solid product, not quite MVP

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from 'axios';
+
+const BASE_URL = 'https://api.nasa.gov';
+const API_KEY = 't1miILVoljZ4KJES25OPAA3ZJCYyCSeLrBEcZuTo'
 
 function App() {
+
+  const [apodData, setApodData] = useState({});
+
+  useEffect(() => {
+    const fetchAPOD = () => {
+    axios
+      .get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}`)
+      .then((res) => {
+        setApodData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+    fetchAPOD();
+  }, []);
+
+  const Content = (props) => (
+        <div>
+            <h1>{props.title}</h1>
+            <img src={props.url} alt={props.title}/>
+        </div>
+
+  );
+
   return (
     <div className="App">
+      <Content url={apodData.url} title={apodData.title} />
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+        {apodData.explanation}
       </p>
     </div>
   );
